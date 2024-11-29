d54aa9e921cda6820875dcc44c59a2c144726fcd
Mohan1330 <mohanraj.krish13@gmail.com>
2024-11-29 18:56:07 +0530

    Add Styles to The LoginScreen

--- src/components/Screens/LoginScreen.jsx
+++ src/components/Screens/LoginScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { Link, useNavigate } from 'react-router-dom';
 import './LoginScreenStyle.css';
 import user_icon from '../Assets/user.png';
@@ -14,6 +14,11 @@ export default function LoginScreen() {
 
     //     // Navigate to HomeScreen
     //     navigate("/HomeScreen");
+
+
+
+    // const [ action, setAction] = useState("Sign Up");
+    const [ action, setAction] = useState("Login");
     // };
     return (
         // <div className='Login-Container'>
@@ -46,22 +51,28 @@ export default function LoginScreen() {
         // </div>
         <div className='Container'>
             <div className="header">
-                <div className="text">SignUp</div>
+                <div className="text">{action}</div>
                 <div className="underline"></div>
             </div>
             <div className="inputs">
                 <div className="input">
                     <img src={user_icon} alt="" />
-                    <input type="text" />
+
+                    <input type="text" placeholder='Name' />
                 </div>
                 <div className="input">
                     <img src={email_icon} alt="" />
-                    <input type="email" />
+                    <input type="email" placeholder='Email' />
                 </div>
                 <div className="input">
                     <img src={password_icon} alt="" />
-                    <input type="password" />
+                    <input type="password" placeholder='Password' />
                 </div>
+            </div>
+            <div className="forgot-password">Forgot Pasword ?<span>click Here !</span></div>
+            <div className="submit_container">
+                <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => {setAction("Sign Up")}}>Sign Up</div>
+                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => {setAction("Login")}}>Login</div>
             </div>
 
 
